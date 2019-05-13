1096c8771c6a7fad70389cecaa85295379bfb99e
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2019-05-13 17:23:35 +0200

    this code is now working baby!! optimisation à 100% de l'affichege 2x2 de la gallerie (bouclé les 12 ImagesIcon, les 12 JLabel, les 12 add!!)

--- src/Smartphone/Gallery.java
+++ src/Smartphone/Gallery.java
@@ -2,6 +2,7 @@ package Smartphone;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
 
 public class Gallery extends JPanel {
 
@@ -11,27 +12,47 @@ public class Gallery extends JPanel {
 
         setLayout(new BorderLayout());
 
-        //Ajout d'un panel global
-        //JPanel pane = new JPanel();
-        imgPanel.setLayout(new GridLayout(0, 3));
-       // pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        imgPanel.setLayout(new GridLayout(0, 2));
 
 
 
+        String [] tabFileName = {"src\\pictures\\1.jpg",
+                "src\\pictures\\2.jpg",
+                "src\\pictures\\3.jpg",
+                "src\\pictures\\4.jpg",
+                "src\\pictures\\5.jpg",
+                "src\\pictures\\6.jpg",
+                "src\\pictures\\7.jpg",
+                "src\\pictures\\8.jpg",
+                "src\\pictures\\9.jpg",
+                "src\\pictures\\10.PNG",
+                "src\\pictures\\11.jpg",
+                "src\\pictures\\goals.jpg"};
+
+        JLabel label[] = new JLabel[12];
+        ImageIcon img [] = new ImageIcon[12];
+
+        for (int i = 0 ; i < tabFileName.length ; i++){
+            img [i] = new ImageIcon(tabFileName[i]);
+            label[i] = new JLabel(img [i]);
+            imgPanel.add(label[i]);
+        }
 
 
+
+        /*
         //Ajout de chaque image dans un label
         ImageIcon image1 = new ImageIcon("src\\pictures\\1.jpg");
         JLabel label1 = new JLabel(image1);
-        label1.setPreferredSize(new Dimension(160, 500));
+
 
         ImageIcon image2 = new ImageIcon("src\\pictures\\2.jpg");
         JLabel label2 = new JLabel(image2);
-        label2.setPreferredSize(new Dimension(160, 500));
+
 
         ImageIcon image3 = new ImageIcon("src\\pictures\\3.jpg");
         JLabel label3 = new JLabel(image3);
-        label3.setPreferredSize(new Dimension(160, 500));
+        //label3.setPreferredSize(new Dimension(160, 500));
 
         ImageIcon image4 = new ImageIcon("src\\pictures\\4.jpg");
         JLabel label4 = new JLabel(image4);
@@ -51,50 +72,33 @@ public class Gallery extends JPanel {
         ImageIcon image9 = new ImageIcon("src\\pictures\\9.jpg");
         JLabel label9 = new JLabel(image9);
 
-        ImageIcon image10 = new ImageIcon("src\\pictures\\9.PNG");
+        ImageIcon image10 = new ImageIcon("src\\pictures\\10.PNG");
         JLabel label10 = new JLabel(image10);
 
-        ImageIcon image11 = new ImageIcon("src\\pictures\\9.jpg");
+        ImageIcon image11 = new ImageIcon("src\\pictures\\11.jpg");
         JLabel label11 = new JLabel(image11);
 
-        ImageIcon image12 = new ImageIcon("src\\pictures\\9.jpg");
+        ImageIcon image12 = new ImageIcon("src\\pictures\\goals.jpg");
         JLabel label12 = new JLabel(image12);
 
-
-
-        /*
-        // Setting up the scroll pane
-        JScrollPane scrollPane = new JScrollPane(panel);
-        scrollPane.setViewportView(label1);
-        scrollPane.setBounds(0, 0, 500, 500);
-        frame.add(scrollPane);
 */
 
 
-        //Ajout des images au panel respectif
-        imgPanel.add(label1);
-        imgPanel.add(label2);
-        imgPanel.add(label3);
-        imgPanel.add(label4);
-        imgPanel.add(label5);
-        imgPanel.add(label6);
-        imgPanel.add(label7);
-        imgPanel.add(label8);
-        imgPanel.add(label9);
-        imgPanel.add(label10);
-        imgPanel.add(label11);
+        /*
+        ImageIcon image12 = new ImageIcon("src\\pictures\\goals.jpg");
+        JLabel label12 = new JLabel(image12);
+        label12.addMouseListener(new MouseListener());
         imgPanel.add(label12);
+*/
 
-        //add(pane, new GridLayout(3,3));
-       // add(pane, BorderLayout.NORTH);
+
+
+
+
         JScrollPane scroll = new JScrollPane();
         scroll.setViewportView(imgPanel);
         add(new JScrollPane(imgPanel));
 
-        //Jpanel.setViewport
-
-
-        //add(panel);
 
     }
 
